580f5216b6cf521bd7fa46280b0a90e7f19ffc2b
Karol Auguštin <karol@augustin.pl>
2012-12-29 00:38:48 +0100

    Config changes to make it less neurodebian

--- cmdsettings.sh
+++ cmdsettings.sh
@@ -2,24 +2,23 @@ family=$1
 dist=$2
 
 # basic settings
-cowbuilderroot="/home/neurodebian"
+cowbuilderroot="/home/karolaug/Work/neurodebian-build"
 buildplace="${cowbuilderroot}/build"
 
 # all currently supported dists
-allnddists="nd+debian-squeeze nd+debian-wheezy nd+debian-sid \
-            nd+ubuntu-hardy nd+ubuntu-karmic nd+ubuntu-lucid nd+ubuntu-maverick nd+ubuntu-natty nd+ubuntu-oneiric nd+ubuntu-precise nd+ubuntu-quantal"
-alldists="$allnddists debian-squeeze debian-wheezy debian-sid"
+allnddists="nd+debian-wheezy nd+ubuntu-precise nd+ubuntu-quantal"
+alldists="$allnddists debian-sid"
 
 # default is debian
 aptcache="${cowbuilderroot}/debian_aptcache"
 components="main contrib non-free"
-mirror="http://debian.lcs.mit.edu/debian"
+mirror=" http://ftp.pl.debian.org/debian"
 
 # overwrite necessary bits for ubuntu
 if [ "${family#nd+}" = "ubuntu" ]; then
   aptcache="${cowbuilderroot}/ubuntu_aptcache"
   components="main universe multiverse"
-  mirror="http://ubuntu.media.mit.edu/ubuntu"
+  mirror="http://pl.archive.ubuntu.com/ubuntu"
 fi
 
 if [ ! -d $aptcache ]; then mkdir -p $aptcache; fi
